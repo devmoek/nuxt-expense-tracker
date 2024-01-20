6411a9fe0ac4ca25320cb3617f543836cfdf8b05
Michael Myers <moekdeveloper@gmail.com>
2024-01-20 11:41:21 +0400

    fix the url in login redirect

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -6,7 +6,7 @@ const signInWithOAuth = async () => {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "github",
     options: {
-      redirectTo: "http://localhost:3000/confirm",
+      redirectTo: "https://nuxt-expense-tracker-cycvintm3-moek.vercel.app/confirm",
     },
   });
   if (error) console.log(error);
