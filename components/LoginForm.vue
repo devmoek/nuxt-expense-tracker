d9cd9f72274059b7bfc6fac5e246f29510625e6b
Michael Myers <moekdeveloper@gmail.com>
2024-01-20 11:50:41 +0400

    hotfix

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -6,7 +6,7 @@ const signInWithOAuth = async () => {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "github",
     options: {
-      redirectTo: "https://nuxt-expense-tracker-cycvintm3-moek.vercel.app/confirm",
+      redirectTo: "https://nuxt-expense-tracker.vercel.app/confirm",
     },
   });
   if (error) console.log(error);
